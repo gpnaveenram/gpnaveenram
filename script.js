a8701db937e01a0f6f7aaf507918bb811a0b4d8c
gpnaveenram <96829467+gpnaveenram@users.noreply.github.com>
2023-04-09 12:32:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function foo(){
+
+    console.log("FirstName is --> ", document.getElementById("fname").value);
+    console.log("MiddleName is --> ", document.getElementById("mname").value);
+    console.log("LastName is --> ", document.getElementById("lname").value);
+    console.log("Email ID is --> ", document.getElementById("email").value);
+   
+}
