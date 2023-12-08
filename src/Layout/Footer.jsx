9efc797a28972349bbf9aa6f9fa2e74ed9c55914
gpnaveenram <96829467+gpnaveenram@users.noreply.github.com>
2023-12-08 23:49:06 +0530

    Update Footer.jsx

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -4,12 +4,14 @@ import styles from './Footer.module.css';
 
 function Footer() {
     return (
-        <div className={styles.footer}>
-
-            Copyrights Reserverd
-
-        </div>
+        <footer className={styles.footer}>
+            <div style={{ margin: '0 auto' }} className='container-fluid'>
+                <p>&copy; 2023 e-Shooping. All rights reserved.</p>
+                <p>Contact: gpnaveenram@gmail.com</p>
+            </div>
+        </footer>
     );
+
 
 }
 
